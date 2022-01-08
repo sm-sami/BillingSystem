3a568b7c4a9973d61b033d8fe21aa93df046b76d
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-01-08 22:45:29 +0530

    Add makeInvoice

--- main.c
+++ main.c
@@ -2,6 +2,50 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int addItem(FILE *bill){
+    printf("Enter PID:");
+    char PID[5];
+    scanf("%s", PID);
+    printf("Quantity:");
+    int quantity;
+    scanf("%d", &quantity);
+    int price = 50 * quantity;
+    fprintf(bill, "\n%s\t%d\t\t\t%d", PID, quantity, price);
+    return price;
+}
+
+void printInvoice(FILE *bill, int total){
+    fprintf(bill, "\nTotal\t\t\t\t%d", total);
+    fclose(bill);
+}
+
+void makeInvoice(){
+    bool toPrint = false;
+    FILE *bill;
+    int total = 0;
+    bill = fopen("bill.txt", "w");
+    fprintf(bill, "\t\tINVOICE\nPID\t\tQuantity\tPrice");
+    while (!toPrint) {
+        printf("\n[1] Add Item\n[2] Print Bill\nChoose an option:");
+        int option;
+        scanf("%d", &option);
+        switch (option) {
+            case 0:
+                return;
+            case 1:
+                total += addItem(bill);
+                break;
+            case 2:
+                toPrint = true;
+                printInvoice(bill, total);
+                break;
+            default:
+                printf("\nInvalid Option");
+                break;
+        }
+    }
+}
+
 int main() {
     printf("\t\tBilling System");
     while (true) {
@@ -12,7 +56,7 @@ int main() {
             case 0:
                 exit(0);
             case 1:
-                //TODO: makeInvoice();
+                makeInvoice();
                 break;
             case 2:
                 //TODO: addProduct;
